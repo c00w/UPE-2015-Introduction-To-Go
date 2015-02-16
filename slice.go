cfd2bc2c3b9eeffc045d9ec548b7eac8859b06f0
Colin L. Rice <colin@daedrum.net>
2015-02-16 18:43:19 -0500

    Clean up presentation for UPE stuff

--- slice.go
+++ slice.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"foo"
 	"net/http"
 )
 
 func main() {
 	a := make([]byte, 10)
-	foo.Fill(a, 10)
-	fmt.Printf("Capacity of a is %d\n", cap(a))
+	fmt.Printf("Capacity of a is %d Length is %d\n", cap(a), len(a))
+	b := make([]byte, 0, 9001)
+	fmt.Printf("Capacity of b is %d Length is %d\n", cap(b), len(b))
 
 	c := append(a, 'h', 'i', '!')
 	c = append(c, c...)
 
-	fmt.Printf("Capacity of c is %d\n", cap(c))
-	fmt.Printf("C is \"%s\"\n", string(c))
+	fmt.Printf("Capacity of c is %d Length is %d\n", cap(c), len(c))
+	fmt.Printf("c is %q\n", string(c))
 
 	resp, _ := http.Get("http://rcos.rpi.edu")
 	defer resp.Body.Close()
 
-	n, _ := resp.Body.Read(c[0:20])
-	fmt.Printf("32 bytes read = \"%s\"\n", string(c[0:n]))
+	n, _ := resp.Body.Read(c[0:32])
+	fmt.Printf("%d bytes read = %q\n", n, string(c[0:n]))
 }
